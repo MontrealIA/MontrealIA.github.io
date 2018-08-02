86def872fa588a41972f9e8e522f2cb541337711
montrealai <montrealai@montrealais-iMac.local>
2018-08-02 16:46:13 -0400

    Site updated: 2018-08-02 16:46:10

--- js/sketch.js
+++ js/sketch.js
@@ -14,7 +14,7 @@ function setup() {
 	frameRate(60);
 	rectMode(CENTER);
 	textSize(18)
-	fill(255);
+	fill(000);
 
 	// Initialize Generation
 	generation.initialize(Person);
@@ -35,39 +35,6 @@ function setup() {
 		generation.evolve();
 		settled = false;
 	}, generationPeriod * 1000);
-
-	/** Input fields & Button to save and load models */
-	const input_field = createElement('textarea');
-	const save_button = createButton('Save Model');
-	const load_button = createButton('Load Model');
-	const download_button = createButton('Download Model');
-
-	input_field.size(width, height * 0.25);
-	input_field.style('cols', '200');
-
-	save_button.mousePressed(() => {
-
-		// Get the best creature
-		let best_creature = generation.getBest();
-		
-		// Show its data in the textarea
-		let json_data = best_creature.brain.toJson();
-		input_field.value(json_data);
-	});
-
-	load_button.mousePressed(() => {
-		let data = input_field.value();
-		let json_data = JSON.parse(data);
-		generation.species.forEach((creature) => {
-			creature.brain.loadFromJson(json_data);
-		})
-	})
-
-	download_button.mousePressed(() => {
-		let best_creature = generation.getBest();
-		let json_data = best_creature.brain.toJson();
-		saveJSON(JSON.parse(json_data), 'best.json')
-	})
 
 	// Run the renderer
 	// let render = Render.create({
@@ -106,30 +73,6 @@ function draw() {
 			creature.think(boundary);
 		}
 	});
-
-	// Display Stats
-	textSize(18)
-	fill("red");
-	text("Generation: " + generation.generation, 40, 50);
-	text("HighScore: " + generation.high_score.toFixed(2), 40, 70);
-	text("Average Score " + generation.avg_score.toFixed(2), 40, 90);
-	text("Population: " + generation.population, 40, 110);
-	text("Generation Period: " + generationPeriod + " seconds", 40, 130);
-	text("Mutation Rate: " + 5 + "%", 40, 150);
-	text("Progress: " + generation.progress.toFixed(2), 40, 170);
-	
-	// Display Inheritance
-	textSize(14);
-	fill('green');
-	text("Creature\tParentA\t\tParentB", width * 0.78, 40)
-	generation.species.forEach((creature, index) => {
-		let txt = '';
-		if (creature.parents.length !== 0)
-			txt = `${creature.id} \t\t\t ${creature.parents[0].id} (${creature.parents[0].score.toFixed(0)}) \t\t\t ${creature.parents[1].id}(${creature.parents[1].score.toFixed(0)})`;
-		else
-			txt = `${creature.id} \t\t\t ------ \t\t\t ------`
-		text(txt, width * 0.80, 60 + (15 * index));
-	})
 
 	// Run Matter-JS Engine
 	Matter.Engine.update(engine);
